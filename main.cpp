d908d6631bf73e55d0a3cf5aab05a0a0ca60410b
Christian Muehlhaeuser <muesli@gmail.com>
2014-01-22 09:57:24 +0100

    * Cleaned up code.

--- main.cpp
+++ main.cpp
@@ -3,19 +3,42 @@
 
 #include <pHash.h>
 
+static const QString TMPNAME = "xraytmp";   // name of temporary dir (usually below /tmp)
+static const int THRESHOLD = 3;             // find at least x dupe snapshots to consider video duped
+static const int KEYFRAMES = 5;             // how many snapshots (keyframes) to compare
+
+
+// create snapshots from video file
+void
+createSnaps( const QString& file, const QString& tmpPath )
+{
+    QStringList args;
+    args << "-sstep" << "1"                             // only output keyframes
+         << "-nosound" << "-vo" << "jpeg"               // output as jpegs
+         << "-ss" << "00:01:00"                         // start one minute into the video
+         << "-frames" << QString::number( KEYFRAMES )   // output 10 frames
+         << file;
+
+    QProcess p;
+    p.setWorkingDirectory( tmpPath );
+    p.start( "mplayer", args );
+    p.waitForFinished( 60000 );             // wait one minute at most
+}
+
+
+// create perceptual hash for snapshots
 QHash<ulong64, int>
-hashDir( const QString& path )
+hashSnaps( const QString& path )
 {
     QHash<ulong64, int> hashes;
+
     QDir dir( path );
     QFileInfoList list = dir.entryInfoList();
     for ( int i = 0; i < list.size(); ++i )
     {
         QFileInfo fileInfo = list.at( i );
-        if ( !fileInfo.fileName().endsWith(".jpg") && !fileInfo.fileName().endsWith(".png") )
+        if ( !fileInfo.fileName().endsWith( ".jpg" ) )
             continue;
-
-//        qDebug() << QString( "%1 %2" ).arg( fileInfo.size(), 10 ).arg( fileInfo.fileName() );
 
         ulong64 hash;
         int err = ph_dct_imagehash(fileInfo.absoluteFilePath().toLocal8Bit(), hash);
@@ -23,6 +46,8 @@ hashDir( const QString& path )
             continue;
 
         hashes.insertMulti( hash, i );
+
+        // delete snapshot
         QFile::remove( fileInfo.absoluteFilePath() );
     }
 
@@ -30,25 +55,113 @@ hashDir( const QString& path )
 }
 
 
-void
-createSnaps( const QString& path )
+// finds similar hashes by hamming distance
+QList <QPair <int, QString> >
+closestHashes( QHash<ulong64, QPair<int, QString> >& hashes, const ulong64& searchKey )
 {
-    QDir::temp().mkdir( "xrayfoo" );
-    QStringList args;
-    args << "-sstep" << "1" << "-nosound" << "-vo" << "jpeg" << "-ss" << "00:00:30" << "-frames" << "10" << path;
+    QList <QPair <int, QString> > res;
 
-    QString newpath = QDir::tempPath() + "/xrayfoo";
-    QProcess p;
-    p.setWorkingDirectory( newpath );
-    p.start( "mplayer", args );
-    p.waitForFinished();
+    for ( int i = 0; i < hashes.keys().count(); i++ )
+    {
+        ulong64 key = hashes.keys().at( i );
+        if ( ph_hamming_distance( searchKey, key ) < 10 )
+        {
+            QPair<int, QString> pair = hashes.values().at( i );
+            res << pair;
+        }
+    }
+
+    return res;
 }
 
 
-bool
-keyLessThan( const QString& a, const QString& b )
+void
+scanDir( QHash<ulong64, QPair<int, QString> >& hashes, const QString& path )
 {
-    return QString::localeAwareCompare( a, b ) < 0;
+//    qDebug() << "Scanning dir:" << path;
+
+    QDir dir( path );
+    QFileInfoList list = dir.entryInfoList();
+    for ( int i = 0; i < list.size(); ++i )
+    {
+        QFileInfo fileInfo = list.at( i );
+        if ( fileInfo.isSymLink() )
+            continue;
+        if ( fileInfo.fileName().startsWith( "." ) )
+            continue;
+
+        if ( fileInfo.isDir() )
+        {
+            // scan recursively
+            scanDir( hashes, fileInfo.absolutePath() + "/" + fileInfo.fileName() );
+            continue;
+        }
+
+        if ( !fileInfo.fileName().endsWith(".wmv") && !fileInfo.fileName().endsWith(".avi") &&
+             !fileInfo.fileName().endsWith(".mp4") && !fileInfo.fileName().endsWith(".mkv") &&
+             !fileInfo.fileName().endsWith(".flv") && !fileInfo.fileName().endsWith(".mpg") &&
+             !fileInfo.fileName().endsWith(".m4v") && !fileInfo.fileName().endsWith(".mov") &&
+             !fileInfo.fileName().endsWith(".divx") && !fileInfo.fileName().endsWith(".mpeg") )
+        {
+            // only index videos
+            continue;
+        }
+
+        std::cout << "Indexing \"" << qPrintable( fileInfo.absoluteFilePath() ) << "\" - size: " << fileInfo.size() << std::endl;
+
+        // create snapshots
+        const QString tmpPath = QDir::tempPath() + "/" + TMPNAME;
+        createSnaps( fileInfo.absoluteFilePath(), tmpPath );
+        // analyze and hash snapshots
+        QHash<ulong64, int> hash = hashSnaps( tmpPath );
+
+        QPair<int, QString> pair;       // snapshot #, filename
+        QHash<QString, int> matches;    // filename, matches
+
+        // go through all extracted snapshots and find dupes
+        for ( int j = 0; j < hash.keys().count(); j++ )
+        {
+            ulong64 key = hash.keys().at( j );  // hash
+            int value = hash.values().at( j );  // snapshot #
+
+            {
+                QList<QPair<int, QString> > res;    // snapshot #, filename
+
+                // find closest snapshots by hamming distance
+                res = closestHashes( hashes, key );
+                for ( int x = 0; x < res.count(); x++ )
+                {
+                    pair = res.at( x );
+                    if ( pair.second == fileInfo.absoluteFilePath() )
+                    {
+                        // ignore matches to self
+                        continue;
+                    }
+
+                    // add/increment match count
+                    if ( !matches.contains( pair.second ) )
+                        matches[pair.second] = 1;
+                    else
+                        matches[pair.second] = matches[pair.second] + 1;
+                }
+            }
+
+            // store hashes for future comparisons
+            pair.first = value;
+            pair.second = fileInfo.absoluteFilePath();
+            hashes.insertMulti( key, pair );
+        }
+
+        for ( int y = 0; y < matches.keys().count(); y++ )
+        {
+            // threshold of matched snapshots to consider duped
+            if ( matches.values().at( y ) >= THRESHOLD )
+            {
+                std::cout << "   --> Dupe of \"" << qPrintable( matches.keys().at( y ) ) << "\" - size: " << QFileInfo( matches.keys().at( y ) ).size()
+                          << " (scores " << qMin( 10, matches.values().at( y ) ) << " out of " << KEYFRAMES << ")" << std::endl;
+            }
+        }
+    }
 }
 
 
@@ -59,94 +172,18 @@ main(int argc, char *argv[])
 
     if ( app.arguments().count() < 2 )
     {
-        std::cout << "First param needs to be a directory that you want to be scanned for duplicate videos." << std::endl;
+        std::cout << "Usage: xray /some/dir/full/of/videos" << std::endl;
+        std::cout << "xray indexes video files by their perceptual hash and finds duplicates." << std::endl
+                  << "This means files are not compared byte for byte, but by their visual content." << std::endl
+                  << "It will/should find dupes of videos, even when they are encoded in different formats and resolutions." << std::endl;
         return 1;
     }
 
-    QHash<ulong64, QPair<int, QString> > hashes;
+    QHash<ulong64, QPair<int, QString> > hashes;    // perceptual hash, pair<snapshot #, filename>
 
-    QDir dir( app.arguments().at( 1 ) );
-    QFileInfoList list = dir.entryInfoList();
-    for ( int i = 0; i < list.size(); ++i )
-    {
-        QFileInfo fileInfo = list.at( i );
-        if ( !fileInfo.fileName().endsWith(".wmv") && !fileInfo.fileName().endsWith(".avi") &&
-             !fileInfo.fileName().endsWith(".mp4") && !fileInfo.fileName().endsWith(".mkv") &&
-             !fileInfo.fileName().endsWith(".flv") && !fileInfo.fileName().endsWith(".mpg") )
-            continue;
-
-        qDebug() << QString( "%1 %2" ).arg( fileInfo.size(), 10 ).arg( fileInfo.fileName() );
-
-        createSnaps( fileInfo.absoluteFilePath() );
-        QHash<ulong64, int> hash = hashDir( QDir::tempPath() + "/xrayfoo" );
-
-        QPair<int, QString> pair;
-        for ( int j = 0; j < hash.keys().count(); j++ )
-        {
-            ulong64 key = hash.keys().at( j );
-            int value = hash.values().at( j );
-
-            if ( hashes.contains( key ) )
-            {
-                QList<QPair<int, QString> > res;
-                res = hashes.values( key );
-
-                for ( int x = 0; x < res.count(); x++ )
-                {
-                    pair = res.at( x );
-                    if ( pair.second == fileInfo.absoluteFilePath() )
-                        continue;
-
-                    qDebug() << "FOUND DUPE:" << value << pair.first << pair.second;
-                }
-            }
-
-            pair.first = value;
-            pair.second = fileInfo.absoluteFilePath();
-            hashes.insertMulti( key, pair );
-        }
-    }
-
-/*    for ( int i = 0; i < hashes.keys().count(); i++ )
-    {
-        QHash<ulong64, QString> h1 = hashes.values().at( i );
-        qDebug() << "Comparing" << i << "-" << hashes.keys().at( i );
-        QHash<QString, bool> found;
-        for ( int x = 0; x < h1.keys().count(); x++ )
-        {
-//            qDebug() << "Comparing" << x << "-" << h1.keys().at( x ) << h1.values().at( x );
-            found[h1.values().at( x )] = false;
-
-            for ( int j = i + 1; j < hashes.keys().count(); j++ )
-            {
-                QHash<ulong64, QString> h2 = hashes.values().at( j );
-//                qDebug() << "    with" << j << "-" << hashes.keys().at( j );
-                for ( int y = 0; y < h2.keys().count(); y++ )
-                {
-                    int distance = ph_hamming_distance( h1.keys().at( x ), h2.keys().at( y ) );
-                    if ( distance < 6 )
-                    {
-//                        qDebug() << "    Exact copy of" << y << "-" << h2.keys().at( y ) << h2.values().at( y );
-                        found[h1.values().at( x )] = true;
-                        qDebug() << "DUPE DETECTED!";
-                        qDebug() << "    with" << j << "-" << hashes.keys().at( j );
-                        break;
-                    }
-                }
-            }
-        }
-
-        int c = 0;
-        QList<QString>fs = found.keys();
-        qSort( fs.begin(), fs.end(), keyLessThan );
-        foreach ( const QString& k, fs )
-        {
-            if ( found.value( k ) )
-                c++;
-
-//            qDebug() << "Found" << k << found.value( k );
-        }
-    }*/
+    QDir::temp().mkdir( TMPNAME );
+    scanDir( hashes, app.arguments().at( 1 ) );
+    QDir::temp().rmdir( TMPNAME );
 
     return 0;
 }
